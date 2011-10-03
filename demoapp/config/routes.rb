52cbcdb572f6c14a850df1c76ae01c1ebcd8cf7a
David Demaree <ddemaree@gmail.com>
2011-10-03 17:16:20 -0500

    No longer storing Merb app in an instance variable

--- demoapp/config/routes.rb
+++ demoapp/config/routes.rb
@@ -3,13 +3,9 @@ Demoapp::Application.routes.draw do
   # Request for Novelties are handled by Rails
   get "/novelties" => "novelties#index", :as => :novelties
 
-  # Storing our Merb instance in a variable so the router won't
-  # try to reinitialize it all the time
-  @merb_application = Merb::Rack::Application.new
-
   # Any requests not explicitly handled by a Rails route
   # above is handled by the Merb app
-  match "*wildcard" => @merb_application
+  match "*wildcard" => Merb::Rack::Application.new
 
   # The application home page is handled by Rails
   root :to => "home#index"
